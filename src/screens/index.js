fe720c7ec724e492436496670efbf91f1ff55fbc
newage10 <newagemindz11@gmail.com>
2023-10-28 10:23:04 +0700

    Setup project

---
+++ src/screens/index.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '@screens/home/HomeScreen';
+import DetailsScreen from '@screens/home/DetailsScreen';
+
+const Stack = createNativeStackNavigator();
+
+const ScreensContainer = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default ScreensContainer;
+
+const styles = StyleSheet.create({});
